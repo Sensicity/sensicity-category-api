9beb2ba255826d7637e3f92e03e0b0419696fcf0
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2017-02-12 10:14:29 +0100

    Add output JSON domain classes for category and category identifiers

---
+++ src/main/scala/models/Categories.scala
@@ -0,0 +1,6 @@
+package models
+
+/**
+  * Represents a set of categories to be returned to the user.
+  */
+case class Categories(categories: Set[String])
---
+++ src/main/scala/models/CategoryIdentifiers.scala
@@ -0,0 +1,6 @@
+package models
+
+/**
+  * Represents a set of category identifiers to be returned to the user.
+  */
+case class CategoryIdentifiers(categories: Set[String])
