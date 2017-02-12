6538667a5db934ee5fb6e8bf28b0d9b51686dc8e
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2017-02-12 14:23:22 +0100

    Parametrize Redis database location using environmental variable

--- README.md
+++ README.md
@@ -2,18 +2,35 @@
 
 Set of microservices used for adding/removing or querying for categories/labels of any type of element.
 
+## API
+
+There are 4 implemented services:
+
+- Add category(s) to identifier
+- Remove category(s) from identifier
+- Get category(s) from identifier
+- Get all identifiers having a category
+
 ## Run the project using Docker
 
 ```
 $ docker-compose up
 ```
 
+The application will now listen for connections at the port 9090.
+
 ## App testing
 
-For testing the App, on the root folder:
+Redis must be running on localhost, for instance, using the command `redis-server`.
+
+If Redis is not running at localhost set the following environment variable to the redis host:
+
+  - `SENSICITY_CATEGORY_REDIS_HOST`
+
+Once Redis is loaded, on the root folder:
 
 ```
 $ sbt test
 ```
 
-*Note: Redis must be enabled and accepting connections from localhost (`redis-server`)** 
+*Note: SBT must be installed*
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,7 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
 import http.ServiceRouter
 import redis.RedisClient
 
@@ -15,12 +16,18 @@ object Main extends App with ServiceRouter {
   override implicit val system: ActorSystem = ActorSystem()
   override implicit val ec: ExecutionContext = system.dispatcher
   override implicit val materializer: ActorMaterializer = ActorMaterializer()
-  override def redisConnector: RedisClient = RedisClient(host = "redis", name = "Sensicity-category-api")
 
-  private[this] val httpInterface: String = "0.0.0.0"
-  private[this] val httpPort : Int = 9090
+  // Initialize Redis database
+
+  private[this] def redisHost: String = ConfigFactory.load("redis.conf").getString("redis_host")
+  override val redisConnector: RedisClient = RedisClient(host = redisHost, name = "Sensicity-category-api")
+  println(s"Loaded Redis database located at '$redisHost'")
 
   // Initializes the HTTP services
+
+  private[this] def httpInterface: String = "0.0.0.0"
+  private[this] def httpPort: Int = 9090
+
   println(s"Initializing REST services on: $httpInterface:$httpPort...")
 
   Http().bindAndHandle(
